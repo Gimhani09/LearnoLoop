3758ebba60248635643594dc72b44570f85e0d16
IT22112750 <it22112750@my.sliit.lk>
2025-05-14 20:20:19 +0530

    updat PostRepository

--- demo/src/main/java/com/example/demo/repository/PostRepository.java
+++ demo/src/main/java/com/example/demo/repository/PostRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
-@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-}
+    List<Post> findByStatus(String status);
+    List<Post> findByUserId(String userId);
+} 
